f98ba7831f4f2f9ecb19cd944c5d980afeba5411
talregev <tal.regev@gmail.com>
2015-08-21 23:22:31 +0300

    add more type from macro code.

--- cv_bridge/src/cv_bridge/CvImage.java
+++ cv_bridge/src/cv_bridge/CvImage.java
@@ -122,30 +122,63 @@ public class CvImage
     }
 
     static int getCvType(final String encoding) throws Exception {
+        
+        String lEncoding = encoding.toUpperCase();
 
-        if (encoding.equals("BGR8"))   return CvType.CV_8UC3;
-        if (encoding.equals("MONO8"))  return CvType.CV_8UC1;
-        if (encoding.equals("RGB8"))   return CvType.CV_8UC3;
-        if (encoding.equals("MONO16")) return CvType.CV_16UC1;
-        if (encoding.equals("BGR16"))  return CvType.CV_16UC3;
-        if (encoding.equals("RGB16"))  return CvType.CV_16UC3;
-        if (encoding.equals("BGRA8"))  return CvType.CV_8UC4;
-        if (encoding.equals("RGBA8"))  return CvType.CV_8UC4;
-        if (encoding.equals("BGRA16")) return CvType.CV_16UC4;
-        if (encoding.equals("RGBA16")) return CvType.CV_16UC4;
+        if (lEncoding.equals("BGR8"))   return CvType.CV_8UC3;
+        if (lEncoding.equals("MONO8"))  return CvType.CV_8UC1;
+        if (lEncoding.equals("RGB8"))   return CvType.CV_8UC3;
+        if (lEncoding.equals("MONO16")) return CvType.CV_16UC1;
+        if (lEncoding.equals("BGR16"))  return CvType.CV_16UC3;
+        if (lEncoding.equals("RGB16"))  return CvType.CV_16UC3;
+        if (lEncoding.equals("BGRA8"))  return CvType.CV_8UC4;
+        if (lEncoding.equals("RGBA8"))  return CvType.CV_8UC4;
+        if (lEncoding.equals("BGRA16")) return CvType.CV_16UC4;
+        if (lEncoding.equals("RGBA16")) return CvType.CV_16UC4;
 
         // For bayer, return one-channel
-        if (encoding.equals("BAYER_RGGB8")) return CvType.CV_8UC1;
-        if (encoding.equals("BAYER_BGGR8")) return CvType.CV_8UC1;
-        if (encoding.equals("BAYER_GBRG8")) return CvType.CV_8UC1;
-        if (encoding.equals("BAYER_GRBG8")) return CvType.CV_8UC1;
-        if (encoding.equals("BAYER_RGGB16")) return CvType.CV_16UC1;
-        if (encoding.equals("BAYER_BGGR16")) return CvType.CV_16UC1;
-        if (encoding.equals("BAYER_GBRG16")) return CvType.CV_16UC1;
-        if (encoding.equals("BAYER_GRBG16")) return CvType.CV_16UC1;
+        if (lEncoding.equals("BAYER_RGGB8")) return CvType.CV_8UC1;
+        if (lEncoding.equals("BAYER_BGGR8")) return CvType.CV_8UC1;
+        if (lEncoding.equals("BAYER_GBRG8")) return CvType.CV_8UC1;
+        if (lEncoding.equals("BAYER_GRBG8")) return CvType.CV_8UC1;
+        if (lEncoding.equals("BAYER_RGGB16")) return CvType.CV_16UC1;
+        if (lEncoding.equals("BAYER_BGGR16")) return CvType.CV_16UC1;
+        if (lEncoding.equals("BAYER_GBRG16")) return CvType.CV_16UC1;
+        if (lEncoding.equals("BAYER_GRBG16")) return CvType.CV_16UC1;
 
         // Miscellaneous
-        if (encoding.equals("YUV422")) return CvType.CV_8UC2;
+        if (lEncoding.equals("YUV422")) return CvType.CV_8UC2;
+
+        //macro code
+        if (lEncoding.equals("TYPE_8UC1")) return CvType.CV_8UC1;
+        if (lEncoding.equals("TYPE_8UC2")) return CvType.CV_8UC2;
+        if (lEncoding.equals("TYPE_8UC3")) return CvType.CV_8UC3;
+        if (lEncoding.equals("TYPE_8UC4")) return CvType.CV_8UC4;
+        if (lEncoding.equals("TYPE_8SC1")) return CvType.CV_8SC1;
+        if (lEncoding.equals("TYPE_8SC2")) return CvType.CV_8SC2;
+        if (lEncoding.equals("TYPE_8SC3")) return CvType.CV_8SC3;
+        if (lEncoding.equals("TYPE_8SC4")) return CvType.CV_8SC4;
+        if (lEncoding.equals("TYPE_16UC1")) return CvType.CV_16UC1;
+        if (lEncoding.equals("TYPE_16UC2")) return CvType.CV_16UC2;
+        if (lEncoding.equals("TYPE_16UC3")) return CvType.CV_16UC3;
+        if (lEncoding.equals("TYPE_16UC4")) return CvType.CV_16UC4;
+        if (lEncoding.equals("TYPE_16SC1")) return CvType.CV_16SC1;
+        if (lEncoding.equals("TYPE_16SC2")) return CvType.CV_16SC2;
+        if (lEncoding.equals("TYPE_16SC3")) return CvType.CV_16SC3;
+        if (lEncoding.equals("TYPE_16SC4")) return CvType.CV_16SC4;
+        if (lEncoding.equals("TYPE_32SC1")) return CvType.CV_32SC1;
+        if (lEncoding.equals("TYPE_32SC2")) return CvType.CV_32SC2;
+        if (lEncoding.equals("TYPE_32SC3")) return CvType.CV_32SC3;
+        if (lEncoding.equals("TYPE_32SC4")) return CvType.CV_32SC4;
+        if (lEncoding.equals("TYPE_32FC1")) return CvType.CV_32FC1;
+        if (lEncoding.equals("TYPE_32FC2")) return CvType.CV_32FC2;
+        if (lEncoding.equals("TYPE_32FC3")) return CvType.CV_32FC3;
+        if (lEncoding.equals("TYPE_32FC4")) return CvType.CV_32FC4;
+        if (lEncoding.equals("TYPE_64FC1")) return CvType.CV_64FC1;
+        if (lEncoding.equals("TYPE_64FC2")) return CvType.CV_64FC2;
+        if (lEncoding.equals("TYPE_64FC3")) return CvType.CV_64FC3;
+        if (lEncoding.equals("TYPE_64FC4")) return CvType.CV_64FC4;
+
         throw new Exception("Unrecognized image encoding [" + encoding + "]");
     }
 
@@ -216,17 +249,19 @@ public class CvImage
 
     protected static Encode getFormat(final String encoding)
     {
-        if ((encoding.equals("MONO8") || (encoding.equals("MONO8")))) return Encode.GRAY;
-        if ((encoding.equals("BGR8") || (encoding.equals("BGR8")))) return Encode.BGR;
-        if ((encoding.equals("RGB8") || (encoding.equals("RGB8")))) return Encode.RGB;
-        if ((encoding.equals("BGRA8") || (encoding.equals("BGRA8")))) return Encode.BGRA;
-        if ((encoding.equals("RGBA8") || (encoding.equals("RGBA8")))) return Encode.RGBA;
-        if (encoding.equals("YUV422")) return Encode.YUV422;
+        String lEncoding = encoding.toUpperCase();
 
-        if ((encoding.equals("BAYER_RGGB8") || (encoding.equals("BAYER_RGGB8")))) return Encode.BAYER_RGGB;
-        if ((encoding.equals("BAYER_BGGR8") || (encoding.equals("BAYER_BGGR8")))) return Encode.BAYER_BGGR;
-        if ((encoding.equals("BAYER_GBRG8") || (encoding.equals("BAYER_GBRG8")))) return Encode.BAYER_GBRG;
-        if ((encoding.equals("BAYER_GRBG8") || (encoding.equals("BAYER_GRBG8")))) return Encode.BAYER_GRBG;
+        if ((lEncoding.equals("MONO8")  || (lEncoding.equals("MONO8")))) return Encode.GRAY;
+        if ((lEncoding.equals("BGR8")   || (lEncoding.equals("BGR8")))) return Encode.BGR;
+        if ((lEncoding.equals("RGB8")   || (lEncoding.equals("RGB8")))) return Encode.RGB;
+        if ((lEncoding.equals("BGRA8")  || (lEncoding.equals("BGRA8")))) return Encode.BGRA;
+        if ((lEncoding.equals("RGBA8")  || (lEncoding.equals("RGBA8")))) return Encode.RGBA;
+        if (lEncoding.equals("YUV422")) return Encode.YUV422;
+
+        if ((lEncoding.equals("BAYER_RGGB8") || (lEncoding.equals("BAYER_RGGB8")))) return Encode.BAYER_RGGB;
+        if ((lEncoding.equals("BAYER_BGGR8") || (lEncoding.equals("BAYER_BGGR8")))) return Encode.BAYER_BGGR;
+        if ((lEncoding.equals("BAYER_GBRG8") || (lEncoding.equals("BAYER_GBRG8")))) return Encode.BAYER_GBRG;
+        if ((lEncoding.equals("BAYER_GRBG8") || (lEncoding.equals("BAYER_GRBG8")))) return Encode.BAYER_GRBG;
 
         // We don't support conversions to/from other types
         return Encode.INVALID;
