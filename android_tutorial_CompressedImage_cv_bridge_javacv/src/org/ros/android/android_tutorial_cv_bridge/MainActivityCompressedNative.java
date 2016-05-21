bb377da9d45cc8bf3b2f70efa6e01b219a625903
talregev <tal.regev@gmail.com>
2016-05-21 16:23:20 +0300

    rename app name. rename cv_bridge_native to cv_bridge_javacv

---
+++ android_tutorial_CompressedImage_cv_bridge_javacv/src/org/ros/android/android_tutorial_cv_bridge/MainActivityCompressedNative.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2015, Tal Regev
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Android Sensors Driver nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.ros.android.android_tutorial_cv_bridge;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_imgproc;
+import org.bytedeco.javacpp.opencv_core.Point;
+import org.bytedeco.javacpp.opencv_core.Scalar;
+import org.ros.android.RosActivity;
+import org.ros.message.MessageListener;
+import org.ros.namespace.GraphName;
+import org.ros.node.ConnectedNode;
+import org.ros.node.Node;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMain;
+import org.ros.node.NodeMainExecutor;
+import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
+
+import cv_bridge.CvImage;
+import cv_bridge.Format;
+import sensor_msgs.CompressedImage;
+import sensor_msgs.ImageEncodings;
+
+
+/**
+ * @author Tal Regev
+ */
+public class MainActivityCompressedNative extends RosActivity implements NodeMain{
+
+    protected Publisher<CompressedImage> imagePublisher;
+    protected Subscriber<CompressedImage> imageSubscriber;
+    protected ConnectedNode node;
+    protected static final String TAG = "compressed Tutorial";
+    protected Bitmap bmp;
+    protected ImageView imageView;
+    protected Runnable displayImage;
+
+
+    public MainActivityCompressedNative() {
+        // The RosActivity constructor configures the notification title and ticker
+        // messages.
+        super("compressed_image Tutorial", "compressed_image Tutorial");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        displayImage = new Runnable() {
+            @Override
+            public void run() {
+                // This code will always run on the UI thread, therefore is safe to modify UI elements.
+                imageView.setImageBitmap(bmp);
+            }
+        };
+    }
+
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor) {
+        // At this point, the user has already been prompted to either enter the URI
+        // of a master to use or to start a master locally.
+
+        // The user can easily use the selected ROS Hostname in the master chooser
+        // activity.
+        NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(getRosHostname());
+        nodeConfiguration.setMasterUri(getMasterUri());
+        nodeMainExecutor.execute(this, nodeConfiguration);
+        onResume();
+    }
+
+    @Override
+    public GraphName getDefaultNodeName() {
+        return GraphName.of("android_tutorial_cv_bridge");
+    }
+
+
+    @Override
+    public void onStart(ConnectedNode connectedNode) {
+    this.node = connectedNode;
+    final org.apache.commons.logging.Log log = node.getLog();
+    imagePublisher = node.newPublisher("/image_converter/output_video/compressed", CompressedImage._TYPE);
+    imageSubscriber = node.newSubscriber("/camera/image/compressed", CompressedImage._TYPE);
+    imageSubscriber.addMessageListener(new MessageListener<CompressedImage>() {
+        @Override
+        public void onNewMessage(CompressedImage message) {
+            CvImage cvImage;
+            try {
+                cvImage = CvImage.toCvCopy(message, ImageEncodings.RGBA8);
+            } catch (Exception e) {
+                log.error("cv_bridge exception: " + e.getMessage());
+                return;
+            }
+
+            //make sure the picture is big enough for my circle.
+            if (cvImage.image.rows() > 110 && cvImage.image.cols() > 110) {
+                //place the circle in the middle of the picture with radius 100 and color red.
+                opencv_imgproc.circle(cvImage.image, new Point(cvImage.image.cols() / 2, cvImage.image.rows() / 2), 100, new Scalar(255, 0, 0,0));
+            }
+
+            cvImage.image = cvImage.image.t().asMat();
+            opencv_core.flip(cvImage.image, cvImage.image, 1);
+
+            //from https://code.google.com/p/javacv/issues/detail?id=67
+            bmp = Bitmap.createBitmap(cvImage.image.cols(), cvImage.image.rows(), Bitmap.Config.ARGB_8888);
+            bmp.copyPixelsFromBuffer(cvImage.image.createBuffer());
+            runOnUiThread(displayImage);
+
+            opencv_core.flip(cvImage.image, cvImage.image, 1);
+            cvImage.image = cvImage.image.t().asMat();
+
+            try {
+                imagePublisher.publish(cvImage.toCompressedImageMsg(imagePublisher.newMessage(), Format.JPG));
+            } catch (Exception e) {
+                log.error("cv_bridge exception: " + e.getMessage());
+            }
+        }
+    });
+
+        Log.i(TAG, "called onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onShutdown(Node node) {
+    }
+
+    @Override
+    public void onShutdownComplete(Node node) {
+    }
+
+    @Override
+    public void onError(Node node, Throwable throwable) {
+    }
+}
---
+++ android_tutorial_cv_bridge_javacv/src/org/ros/android/android_tutorial_cv_bridge/MainActivityNative.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2015, Tal Regev
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Android Sensors Driver nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.ros.android.android_tutorial_cv_bridge;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_core.Point;
+import org.bytedeco.javacpp.opencv_core.Scalar;
+import org.bytedeco.javacpp.opencv_imgproc;
+import org.ros.android.RosActivity;
+import org.ros.message.MessageListener;
+import org.ros.namespace.GraphName;
+import org.ros.node.ConnectedNode;
+import org.ros.node.Node;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMain;
+import org.ros.node.NodeMainExecutor;
+import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
+
+import java.io.IOException;
+
+import cv_bridge.CvImage;
+import sensor_msgs.Image;
+import sensor_msgs.ImageEncodings;
+
+
+/**
+ * @author Tal Regev
+ */
+public class MainActivityNative extends RosActivity implements NodeMain{
+
+    protected Publisher<Image> imagePublisher;
+    protected Subscriber<Image> imageSubscriber;
+    protected ConnectedNode node;
+    protected static final String TAG = "cv_bridge Tutorial";
+    protected ImageView imageView;
+    protected Bitmap bmp;
+    protected Runnable displayImage;
+
+
+    public MainActivityNative() {
+        // The RosActivity constructor configures the notification title and ticker
+        // messages.
+        super("cv_bridge Tutorial", "cv_bridge Tutorial");
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        displayImage = new Runnable() {
+            @Override
+            public void run() {
+                // This code will always run on the UI thread, therefore is safe to modify UI elements.
+                imageView.setImageBitmap(bmp);
+            }
+        };
+    }
+
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor) {
+        // At this point, the user has already been prompted to either enter the URI
+        // of a master to use or to start a master locally.
+
+        // The user can easily use the selected ROS Hostname in the master chooser
+        // activity.
+        NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(getRosHostname());
+        nodeConfiguration.setMasterUri(getMasterUri());
+        nodeMainExecutor.execute(this, nodeConfiguration);
+        onResume();
+    }
+
+    @Override
+    public GraphName getDefaultNodeName() {
+        return GraphName.of("android_tutorial_cv_bridge");
+    }
+
+    @Override
+    public void onStart(ConnectedNode connectedNode) {
+    this.node = connectedNode;
+    final org.apache.commons.logging.Log log = node.getLog();
+    imagePublisher = node.newPublisher("/image_converter/output_video/raw", Image._TYPE);
+    imageSubscriber = node.newSubscriber("/camera/image/raw", Image._TYPE);
+    imageSubscriber.addMessageListener(new MessageListener<Image>() {
+        @Override
+        public void onNewMessage(Image message) {
+
+                CvImage cvImage;
+                try {
+                    cvImage = CvImage.toCvCopy(message, ImageEncodings.RGBA8);
+                } catch (Exception e) {
+                    log.error("cv_bridge exception: " + e.getMessage());
+                    return;
+                }
+                //make sure the picture is big enough for my circle.
+                if (cvImage.image.rows() > 110 && cvImage.image.cols() > 110) {
+                    //place the circle in the middle of the picture with radius 100 and color red.
+                    opencv_imgproc.circle(cvImage.image, new Point(cvImage.image.cols() / 2, cvImage.image.rows() / 2), 100, new Scalar(255, 0, 0, 0));
+                }
+
+                cvImage.image =  cvImage.image.t().asMat();
+                opencv_core.flip(cvImage.image, cvImage.image, 1);
+
+                bmp = Bitmap.createBitmap(cvImage.image.cols(), cvImage.image.rows(), Bitmap.Config.ARGB_8888);
+                bmp.copyPixelsFromBuffer(cvImage.image.createBuffer());
+                runOnUiThread(displayImage);
+
+                opencv_core.flip(cvImage.image, cvImage.image, 1);
+                cvImage.image = cvImage.image.t().asMat();
+
+                try {
+                    cvImage = CvImage.cvtColor(cvImage,"rgb8");
+                    imagePublisher.publish(cvImage.toImageMsg(imagePublisher.newMessage()));
+                } catch (Exception e) {
+                    log.error("cv_bridge exception: " + e.getMessage());
+                }
+        }
+    });
+
+        Log.i(TAG, "called onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onShutdown(Node node) {
+    }
+
+    @Override
+    public void onShutdownComplete(Node node) {
+    }
+
+    @Override
+    public void onError(Node node, Throwable throwable) {
+    }
+}
